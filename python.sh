bb8359b28ed64b72fe065fa4212d8768286ecf0b
xando <sebastian.pawlus@gmail.com>
2011-01-09 18:04:58 +0100

    virtualenv and virtualenvwrapper moved to pip install

--- python.sh
+++ python.sh
@@ -1,2 +1,3 @@
 sudo apt-get install -y python-dev python-setuptools
-sudo easy_install -U pip virtualenv virtualenvwrapper 
+sudo easy_install -U pip
+sudo pip install -U virtualenv virtualenvwrapper
