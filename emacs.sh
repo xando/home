a2f9ac09cc1516daf61a37f71f51a92bc78cda17
xando <sebastian.pawlus@gmail.com>
2011-01-09 17:57:34 +0100

    Missed update

--- emacs.sh
+++ emacs.sh
@@ -1,8 +1,9 @@
 # emacs with emacs-starter-k
 sudo add-apt-repository ppa:ubuntu-elisp/ppa
-sudo apt-get install -y git-core
+sudo apt-get update
 sudo apt-get install -y emacs
 
+sudo apt-get install -y git-core
 git clone https://github.com/gabrielelanaro/emacs-starter-kit.git $HOME/.emacs.d
 
 # symbolic links
