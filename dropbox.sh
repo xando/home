0ba15d3c581614f0c4d549ce4847d91234539d05
xando <sebastian.pawlus@gmail.com>
2011-04-30 13:47:07 +0200

    Dropbox package updated

--- dropbox.sh
+++ dropbox.sh
@@ -1,1 +1,4 @@
-sudo apt-get install -y nautilus-dropbox
+wget https://www.dropbox.com/download?dl=packages/nautilus-dropbox_0.6.7_i386.deb -O dropbox.deb
+sudo dpkg -i dropbox.deb
+sudo apt-get update 
+sudo apt-get upgrade -y nautilus-dropbox
